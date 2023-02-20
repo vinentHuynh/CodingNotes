41f532799b745baf11f0fbcea41acce1dbf01df5
vinentHuynh <102928954+vinentHuynh@users.noreply.github.com>
2023-02-20 14:32:11 +0000

    added sync error function

---
+++ sync-function-error.ts
@@ -0,0 +1,26 @@
+export function ValidateEvent(event: any): void {
+  const errors: Error[] = [];
+
+  try {
+    if (true) {
+      throw new Error(`Invalid path parameters:`);
+    }
+  } catch (error: any) {
+    errors.push(error);
+  }
+
+  try {
+    // call function1()
+  } catch (error: any) {}
+
+  try {
+  } catch (error: any) {
+    //call function2
+    errors.push(error);
+  }
+
+  if (errors.length > 0) {
+    // throws all errors found
+    throw errors;
+  }
+}
